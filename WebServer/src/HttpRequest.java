a215a98e0bd5b1dc78158cb81e83acf16bc67aee
ToastedKing <38260176+ToastedKing@users.noreply.github.com>
2019-12-12 13:32:45 -0600

    Update HttpRequest.java

--- WebServer/src/HttpRequest.java
+++ WebServer/src/HttpRequest.java
@@ -8,7 +8,9 @@ public class HttpRequest {
 		//only need the first line the GET / HTTP...
 		String lines[] = request.split("\n");
 		filename = lines[0].split(" ")[1];
-		System.out.println(filename);
+		if (filename.endsWidth("/")){
+			filename += "index.html";
+		}
 		
 	}
 
